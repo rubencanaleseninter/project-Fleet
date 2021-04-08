17e931ab7260b4a3644cd6e9495b33f54b12775d
Ruben Canales - Ascensores Eninter <rubencanales@eninter.com>
2021-04-08 09:09:09 +0200

    skeleton component added

---
+++ src/app/shared/components/skeleton-table/skeleton-table.component.ts
@@ -0,0 +1,9 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-skeleton-table',
+  templateUrl: './skeleton-table.component.html',
+  styleUrls: ['./skeleton-table.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SkeletonTableComponent {}
